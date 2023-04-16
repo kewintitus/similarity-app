5ab7583eae410d6c973bef4ca9a11a698b54a168
Kewin Titus <kewintitus@gmail.com>
2023-04-16 17:20:34 +0530

    created new sign-in button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
 import React from 'react';
+import { buttonVariants } from '@/ui/Button';
+import { SignInButton } from '@/components/SignInButton';
 
 interface NavbarProps {}
 
@@ -9,7 +11,7 @@ const Navbar = async (props: NavbarProps) => {
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark: bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between ">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center ">
-        <Link href="/" className={buttonVariants({ variants: 'link' })}>
+        <Link href="/" className={buttonVariants({ variant: 'link' })}>
           Text Similarity 1.0
         </Link>
         <div className="md:hidden">
---
+++ src/components/SignInButton.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React, { useState } from 'react';
+import Button from './ui/Button';
+import { signIn } from 'next-auth/react';
+
+interface SignInButtonProps {}
+
+export const SignInButton = ({}) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const signInWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn('google');
+    } catch (error) {
+      toast({
+        title: 'Error signing in',
+        message: 'Please try again later',
+        type: 'error',
+      });
+    }
+  };
+
+  return (
+    <Button onClick={signInWithGoogle} isLoading={isLoading}>
+      Sign In
+    </Button>
+  );
+};
