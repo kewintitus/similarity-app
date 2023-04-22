f26080ed70f21e02a4e2997645e9d10656594a14
Kewin Titus <kewintitus@gmail.com>
2023-04-22 22:34:23 +0530

    added app home page

--- src/components/ui/Paragraph.tsx
+++ src/components/ui/Paragraph.tsx
@@ -1,27 +1,28 @@
-import React, { FC, ForwardedRef, HTMLAttributes, forwardRef } from 'react';
+import * as React from 'react';
 import { VariantProps, cva } from 'class-variance-authority';
+
 import { cn } from '@/lib/utils';
 
-const paragraphVariants = cva(
+export const paragraphVariants = cva(
   'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center',
   {
     variants: {
       size: {
-        default: 'text-base sm: text-lg',
-        sm: 'text-sm, sm: text-base',
+        default: 'text-base sm:text-lg',
+        sm: 'text-sm sm:text-base',
       },
-      defaultVariant: {
-        size: 'default',
-      },
+    },
+    defaultVariants: {
+      size: 'default',
     },
   }
 );
 
 interface ParagraphProps
-  extends HTMLAttributes<HTMLParagraphElement>,
+  extends React.HTMLAttributes<HTMLParagraphElement>,
     VariantProps<typeof paragraphVariants> {}
 
-const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+const Paragraph = React.forwardRef<HTMLParagraphElement, ParagraphProps>(
   ({ className, size, children, ...props }, ref) => {
     return (
       <p
